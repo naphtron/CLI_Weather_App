4f23c05ad1156810e5130d2fbe4d55a3e866e5ea
naphtron <naphtron@yahoo.com>
2023-03-01 22:43:42 +0300

    creating functions/end of day tired

--- weather_api.py
+++ weather_api.py
@@ -1,10 +1,39 @@
-import requests
-import json
+# import requests
+# import json
 
-x = requests.get('http://www.7timer.info/bin/astro.php?lon=113.17&lat=23.09&ac=0&lang=en&unit=metric&output=json&tzshift=0')
-data = json.loads(x.text)
-print(data['dataseries'][0]['wind10m']['direction'])
+# x = requests.get('http://www.7timer.info/bin/astro.php?lon=113.17&lat=23.09&ac=0&lang=en&unit=metric&output=json&tzshift=0')
+# data = json.loads(x.text)
+# print(data['dataseries'][0]['wind10m']['direction'])
 
 # Just learning how to consume APIs.
 # API: 7timer weather API
 # Monday 27th February 2023
+
+import argparse
+from configparser import ConfigParser
+
+def _get_api_key() -> str:
+    """Fetch the API key from your configuration file.
+
+    Expects a configuration file named "secrets.ini" with structure:
+
+        [openweather]
+        api_key=<YOUR-OPENWEATHER-API-KEY>
+    """
+    config = ConfigParser()
+    config.read("secrets.ini")
+    return config["openweather"]["api_key"]
+# print(_get_api_key())
+def read_user_cli_args() -> str:
+    """Handles the CLI user interactions.
+
+    Returns:
+        argparse.Namespace: Populated namespace object
+    """
+    parser = argparse.ArgumentParser(
+        description="gets weather and temperature information for a city"
+    )
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    read_user_cli_args()
