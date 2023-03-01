de9a9cab37b231537720c15c5cf51786320dde84
naphtron <naphtron@yahoo.com>
2023-03-01 22:58:29 +0300

    adding arguments

--- weather_api.py
+++ weather_api.py
@@ -33,6 +33,15 @@ def read_user_cli_args() -> str:
     parser = argparse.ArgumentParser(
         description="gets weather and temperature information for a city"
     )
+    parser.add_argument(
+        "city", nargs="+", type=str, help="enter the city name"
+    )
+    parser.add_argument(
+        "-i",
+        "--imperial",
+        action="store_true",
+        help="display the temperature in imperial units",
+    )
     return parser.parse_args()
 
 if __name__ == "__main__":
